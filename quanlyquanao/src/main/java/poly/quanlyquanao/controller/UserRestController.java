a1a6b4723dbc940b086a2b9223f34b0a618a0365
Luong Thuan Thien <luongthuanthien2000@gmail.com>
2025-06-09 16:43:49 +0700

    remove thua thai

--- quanlyquanao/src/main/java/poly/quanlyquanao/controller/UserRestController.java
+++ quanlyquanao/src/main/java/poly/quanlyquanao/controller/UserRestController.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import poly.quanlyquanao.model.User;
-import poly.quanlyquanao.security.CustomUserDetails;
 import poly.quanlyquanao.service.Impl.UserServiceImpl;
 
 @RestController
