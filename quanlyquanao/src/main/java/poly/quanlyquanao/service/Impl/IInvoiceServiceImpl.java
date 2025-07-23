cfb13e12e00b7921d0f101e44fba4422b07964dd
Luong Thuan Thien <luongthuanthien2000@gmail.com>
2025-07-23 16:19:58 +0700

    Tạo hóa đơn người dùng

--- quanlyquanao/src/main/java/poly/quanlyquanao/service/Impl/IInvoiceServiceImpl.java
+++ quanlyquanao/src/main/java/poly/quanlyquanao/service/Impl/IInvoiceServiceImpl.java
@@ -3,9 +3,11 @@ package poly.quanlyquanao.service.Impl;
 import java.util.List;
 
 import poly.quanlyquanao.dto.InvoiceDTO;
+import poly.quanlyquanao.model.Invoice;
 
 public interface IInvoiceServiceImpl {
 	List<InvoiceDTO> getAllInvoice();
 	InvoiceDTO getInvoiceById(Long id);
 	void updateInvoiceStatus(Long id, Integer status);
+	List<Invoice> getInvoicesByCurrentUser(String username);
 }
